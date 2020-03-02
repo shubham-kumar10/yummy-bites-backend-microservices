daf91ec9c4e218a23c7cff8c5e7c871b8f0203ab
Kumar, Shubham <shubham.snu@gmail.com>
2020-03-02 19:40:22 +0530

    auth service init

---
+++ authentication-service/src/main/java/com/cognizant/authenticationservice/AuthenticationServiceApplication.java
@@ -0,0 +1,14 @@
+package com.cognizant.authenticationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+public class AuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationServiceApplication.class, args);
+	}
+
+}
