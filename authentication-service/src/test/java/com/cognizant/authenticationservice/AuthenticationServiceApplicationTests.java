bc1b24a1310c2739a1c497df92efe9db5446dd91
Kumar, Shubham <shubham.snu@gmail.com>
2020-03-03 19:42:49 +0530

    text classes

---
+++ authentication-service/src/test/java/com/cognizant/authenticationservice/AuthenticationServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.cognizant.authenticationservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationServiceApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
