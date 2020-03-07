a82543fdad038a238d5f5a2b045bc5318868d178
Kumar, Shubham <shubham.snu@gmail.com>
2020-03-07 20:34:40 +0530

    Repository classes

---
+++ menuitem-service/src/main/java/com/cognizant/menuitemservice/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.cognizant.menuitemservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cognizant.menuitemservice.model.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+}
