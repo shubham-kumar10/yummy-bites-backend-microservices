05e7e92885e340c024b8864924d0717940f9bfc8
Kumar, Shubham <shubham.snu@gmail.com>
2020-03-07 20:31:01 +0530

    Menuitem service

---
+++ menuitem-service/src/test/java/com/cognizant/menuitemservice/MenuitemServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.cognizant.menuitemservice;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuitemServiceApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
